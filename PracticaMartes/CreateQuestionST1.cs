94a4d1a15b13bedf62dea8647e58fc598b718452
Manu Delgado <manudelgado4@gmail.com>
2018-05-08 10:51:43 +0200

    Changing Console for Debug

--- PracticaMartes/CreateQuestionST1.cs
+++ PracticaMartes/CreateQuestionST1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -44,90 +45,90 @@ namespace PracticaMartes
         [Test]
         public void TheExhibitsFieldsListedTwiceTest()
         {
-            Console.WriteLine("Navegando a ST1");
+            Debug.WriteLine("Navegando a ST1");
             driver.Navigate().GoToUrl("https://examdevtst1.pearsonvue.com/ExamDeveloper/login/Login.aspx");
 
-            Console.WriteLine("Introducimos username");
+            Debug.WriteLine("Introducimos username");
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbUsername")).Click();
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbUsername")).Clear();
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbUsername")).SendKeys("Lynx01");
 
-            Console.WriteLine("Introducimos password");
+            Debug.WriteLine("Introducimos password");
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbPassword")).Click();
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbPassword")).Clear();
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbPassword")).SendKeys("ed1tHPi0f");
 
-            Console.WriteLine("Clickamos en login");
+            Debug.WriteLine("Clickamos en login");
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_btnLogin")).Click();
 
-            Console.WriteLine("Introducimos la respuesta de confirmacion");
+            Debug.WriteLine("Introducimos la respuesta de confirmacion");
             driver.FindElement(By.Id("ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_tbAnswer")).Click();
             driver.FindElement(By.Id("ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_tbAnswer")).Clear();
             driver.FindElement(By.Id("ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_tbAnswer")).SendKeys("velvet");
             driver.FindElement(By.Id("ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_btnSubmit")).Click();
             Thread.Sleep(4000);
 
-            Console.WriteLine("Buscamos el proyecto Manu y lo abrimos");
+            Debug.WriteLine("Buscamos el proyecto Manu y lo abrimos");
             wait.Until(x => x.FindElement(By.XPath("(//img[@alt='#'])[5]"))).Click();
             driver.FindElement(By.LinkText("Manu project")).Click();
 
-            Console.WriteLine("Click on Develop Question");
+            Debug.WriteLine("Click on Develop Question");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_plcMenu_mnuTopMenu_rptTopmenu_ctl00_lnkTopmenu")).Click();
 
-            Console.WriteLine("Click on Write Question");
+            Debug.WriteLine("Click on Write Question");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_btnWrite")).Click();
 
-            Console.WriteLine("Seleccionamos el idioma ingles");
+            Debug.WriteLine("Seleccionamos el idioma ingles");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_ddlLanguageCode")).Click();
             new SelectElement(driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_ddlLanguageCode"))).SelectByText("English");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_ddlLanguageCode")).Click();
 
-            Console.WriteLine("Introducimos el stem");
+            Debug.WriteLine("Introducimos el stem");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_tbQuestionText_dvEditorControl")).Click();
             driver.SwitchTo().Frame("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_tbQuestionText_txtEditorControl_ifr");
-            driver.FindElement(By.Id("tinymce")).SendKeys("Question 3");
+            driver.FindElement(By.Id("tinymce")).SendKeys("Question 4");
             driver.SwitchTo().DefaultContent();
 
-            Console.WriteLine("Introducimos la opcion A");
+            Debug.WriteLine("Introducimos la opcion A");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl00_tbDistractor_dvEditorControl")).Click();
             driver.SwitchTo().Frame("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl00_tbDistractor_txtEditorControl_ifr");
             driver.FindElement(By.Id("tinymce")).SendKeys("A");
             driver.SwitchTo().DefaultContent();
 
-            Console.WriteLine("Introducimos la opcion B");
+            Debug.WriteLine("Introducimos la opcion B");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl01_tbDistractor_dvEditorControl")).Click();
             driver.SwitchTo().Frame("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl01_tbDistractor_txtEditorControl_ifr");
             driver.FindElement(By.Id("tinymce")).SendKeys("B");
             driver.SwitchTo().DefaultContent();
 
-            Console.WriteLine("Introducimos la opcion C");
+            Debug.WriteLine("Introducimos la opcion C");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl02_tbDistractor_dvEditorControl")).Click();
             driver.SwitchTo().Frame("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl02_tbDistractor_txtEditorControl_ifr");
             driver.FindElement(By.Id("tinymce")).SendKeys("C");
             driver.SwitchTo().DefaultContent();
 
-            Console.WriteLine("Introducimos la opcion D");
+            Debug.WriteLine("Introducimos la opcion D");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl03_tbDistractor_dvEditorControl")).Click();
             driver.SwitchTo().Frame("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl03_tbDistractor_txtEditorControl_ifr");
             driver.FindElement(By.Id("tinymce")).SendKeys("D");
             driver.SwitchTo().DefaultContent();
 
-            Console.WriteLine("Marcamos la opcion correcta");
+            Debug.WriteLine("Marcamos la opcion correcta");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_QuestionControl_qEditor1_rptOptions_ctl00_chkDistractor")).Click();
 
-            Console.WriteLine("Clickamos en submit");
+            Debug.WriteLine("Clickamos en submit");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_btnSubmitForReview")).Click();
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_btnSubmit")).Click();
             driver.FindElement(By.Id("__tab_ctl00_ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_RHSContentPlaceHolder_tabs_tabDashboard")).Click();
 
-            Console.WriteLine("Comprobamos que la nueva pregunta se ha creado");
-            Assert.AreEqual("Question 3", driver.FindElement(By.LinkText("Question 3")).Text);
+            Debug.WriteLine("Comprobamos que la nueva pregunta se ha creado");
+            Assert.AreEqual("Question 4", driver.FindElement(By.LinkText("Question 4")).Text);
 
-            Console.WriteLine("Hacemos click on LogOut");
+            Debug.WriteLine("Hacemos click on LogOut");
             driver.FindElement(By.Id("ctl00_ctl00_ctl00_plcMenu_mnuTopMenu_lbtnLogout")).Click();
             driver.FindElement(By.Id("ctl00_RHSContentPlaceHolder_Button1")).Click();
 
-            Console.WriteLine("Comprobamos que nos hemos deslogado");
+            Debug.WriteLine("Comprobamos que nos hemos deslogado");
             Assert.IsTrue(Regex.IsMatch(driver.FindElement(By.XPath("//form[@id='aspnetForm']/div[4]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/center/div[3]/div/div/div/div/div/div[2]/div")).Text, "^[\\s\\S]* Username:$"));
         }
         private bool IsElementPresent(By by)
